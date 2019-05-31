60d1debedf8a10a65d7d838bfeadcae05ec5ea84
Hemesh Reddy <Hemesh.Reddy@cheproximity.com.au>
2019-05-31 21:25:34 +1000

    Updates

---
+++ precache-manifest.b5c45c0cf80ba6a363ef7288ac882de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6971da7cfebc3a33f6ab4af4bbf38f5e",
+    "url": "/color-app/index.html"
+  },
+  {
+    "revision": "f21aa5c8322e89d3a71c",
+    "url": "/color-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d9e5874a1bd278bcbe37",
+    "url": "/color-app/static/js/2.2ba7bb5e.chunk.js"
+  },
+  {
+    "revision": "f21aa5c8322e89d3a71c",
+    "url": "/color-app/static/js/main.507fbf16.chunk.js"
+  },
+  {
+    "revision": "ea958c06051b899540ac",
+    "url": "/color-app/static/js/runtime~main.bf85e199.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/color-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
